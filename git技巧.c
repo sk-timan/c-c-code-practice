5817465803f35312fe1ec3b5965c7cee8250c150
sk-timen <1596120742@qq.com>
2018-07-18 19:33:18 +0800

    s

---
+++ git技巧.c
@@ -0,0 +1,19 @@
+git init 	//本地工作台初始化
+git config --global user.name ''	//本地用户名
+git config --global user.email  	//本地用户邮箱
+
+git status   	//查看缓存区
+git add  	//添加文件到缓存区
+git commit -m ''  	//从缓存区上传到本地库
+
+
+git remote -v  	//查看远程库地址
+git remote add origin(本地自定义远程库地址名称)+http  	//添加远程库地址
+
+
+git push origin(本地自定义远程库地址名称) master(本地分支名) 	//推送
+
+
+
+
+
