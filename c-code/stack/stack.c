04d25ee7aac36c5620a2fbdd9c079d3c907d2c90
sk-timen <1596120742@qq.com>
2018-08-04 17:48:43 +0800

    静态库方法

---
+++ c-code/stack/stack.c
@@ -0,0 +1,61 @@
+/*char stack[512];
+int top = -1;
+
+void push(char c)
+{
+	stack[++top] = c;
+}
+
+char pop(void)
+{
+	return stack[top--];
+}
+
+int is_empty(void)
+{
+	return top == -1;
+}
+*/
+
+/*static char stack[512];    //封装内部变量
+static int top = -1;
+
+
+void push(char c)
+{
+	stack[++top] = c;
+}
+
+char pop(void)
+{
+	return stack[top--];
+}
+
+int is_empty(void)
+{
+	return top == -1;
+}
+*/
+
+char stack[512];
+int top = -1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
